1ed96a74a84f3426661decbb0de5c53b9af98d8d
Justin Vos <justinvosnz@gmail.com>
2023-09-10 20:18:11 +1200

    Add more notes and clean up code

--- src/BinarySearch.ts
+++ src/BinarySearch.ts
@@ -1,8 +1,9 @@
 /**
- *
+ * Binary Searches for needle in haystack
+ * Time complexity: O(log n)
  * @param needle
  * @param haystack assumed to be sorted in ascending order
- * @returns
+ * @returns index of found needle or -1
  */
 export function binarySearch<T>(needle: T, haystack: T[]): number {
   let low = 0;
@@ -11,19 +12,26 @@ export function binarySearch<T>(needle: T, haystack: T[]): number {
   while (low < high) {
     const mid = Math.floor(low + (high - low) / 2);
     const val = haystack[mid];
-    console.log("iter", low, mid, high, val);
 
     if (needle === val) {
       return mid;
-    } else if (needle > val) {
-      // look at second half
-      low = mid + 1;
-    } else {
+    } else if (needle < val) {
+      // look at the first half next
       high = mid;
+    } else {
+      // look at second half next
+      low = mid + 1;
     }
   }
 
   return -1;
 }
 
-console.log("BinarySearch:", binarySearch(3, [1, 2, 3, 4, 5, 6, 7]));
+console.log(
+  "BinarySearch with numbers:",
+  binarySearch(3, [1, 2, 3, 4, 5, 6, 7])
+);
+console.log(
+  "BinarySearch with strings:",
+  binarySearch("g", ["a", "c", "d", "e", "f", "g", "h"])
+);
